f53d01a15189c64db1a45b3e8a3c09d87d16d700
Ivan Ivanov <ivanovcorp0@gmail.com>
2018-08-31 19:32:32 +0300

    Fixed logging

--- fileconsistencychecker/src/main/java/example/Example.java
+++ fileconsistencychecker/src/main/java/example/Example.java
@@ -1,32 +1,29 @@
 package example;
 
 import com.ivanovcorp.consistencychecker.ConsistencyChecker;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Example {
 
   private static final String algorithm = "MD5";
+  private static final Logger LOG = LogManager.getLogger(Example.class);
 
   public static void main(String[] args) {
-    //TODO: fix the issues with Logger
-    System.out.print("Positive case. Are files equal:  ");
-    runPositiveCase();
-    System.out.println();
-
-    System.out.print("Negative case. Are files equal: ");
-    runNegativeCase();
-    System.out.println();
+    LOG.info("Positive case. Are files equal:  " + runPositiveCase());
+    LOG.info("Negative case. Are files equal: " + runNegativeCase());
   }
 
-  private static void runNegativeCase() {
+  private static boolean runNegativeCase() {
     String fileA = "src\\main\\resources\\fail-base-file.txt";
     String fileB = "src\\main\\resources\\fail-target-file.txt";
-    System.out.print(ConsistencyChecker.checkFileConsistency(fileA, fileB, algorithm));
+    return ConsistencyChecker.checkFileConsistency(fileA, fileB, algorithm);
   }
 
 
-  private static void runPositiveCase() {
+  private static boolean runPositiveCase() {
     String fileA = "src\\main\\resources\\success-base-file.txt";
     String fileB = "src\\main\\resources\\success-target-file.txt";
-    System.out.print(ConsistencyChecker.checkFileConsistency(fileA, fileB, algorithm));
+    return ConsistencyChecker.checkFileConsistency(fileA, fileB, algorithm);
   }
 }
